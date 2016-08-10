8be2b198368595d89a54526aacca3c8e663f14f1
Ben Schmoker <bschmoker@users.noreply.github.com>
2016-08-10 07:30:06 -0400

    Update sora.py

--- sora.py
+++ sora.py
@@ -4,7 +4,7 @@ import web
 import psutil
 import json
 
-# define endpoints
+# define our endpoints
 urls = (
     '/ps', 'process_list'
 )
@@ -14,21 +14,23 @@ class process_list:
     def GET(self):
         psjson = {}
         
+        # Get a list of all non-root processes on the local system
         for each_proc in psutil.process_iter():
+            
+            # Pull out process metadata that we want to return
             process = each_proc.as_dict(attrs=['pid', 'name', 'username','ppid','cmdline','environ']) 
             
-            
-            # Handle empty command lines 
+            # Handle empty command lines - treated as 'false' if empty
             fmt_cmdline = False
             if process['cmdline']:
-                # Convert from argv-style list of parameters to space-delimited string
+                # Convert our command line from argv-style list of parameters to space-delimited string
                 fmt_cmdline = ' '.join(process['cmdline'])
                 
-            # skip the agent process
+            # skip the agent process - don't show it in process results 
             if "sora.py" in fmt_cmdline:
                 continue
             
-            # convert dict of environment variables to list
+            # convert dict of environment variables to a list
             if process.get('environ') is not None:
                 fmt_environs = []
                 
@@ -39,7 +41,7 @@ class process_list:
             else:
                 fmt_environs = None
             
-            # build custom dictionary
+            # build dict to be returned as json
             psdict = {
                 process['pid']:
                     {
@@ -50,12 +52,14 @@ class process_list:
                     }
                 } 
             
-            
+            # add our dict to the overall
             psjson.update(psdict)
         
+        # endfunc
         return json.dumps (psjson, indent=4, sort_keys=True)
 
 if __name__ == "__main__":
     # run our app - listening on a given port
     app = web.application(urls, globals())
+    # note that SSL is not currently supported
     web.httpserver.runsimple(app.wsgifunc(), ("0.0.0.0", 1337))
